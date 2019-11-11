8c687dbed0795d0ccaae7d48d9a1c21780ed48b9
SrinuNp <SrinuNp111@gmail.com>
2019-11-11 11:03:03 +0530

    Adding CrossOrigin Annotation to Support Angular

--- session2/RestfulEmpApp/src/main/java/com/datageeks/controller/EmployeeController.java
+++ session2/RestfulEmpApp/src/main/java/com/datageeks/controller/EmployeeController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.datageeks.dao.EmployeeBean;
@@ -18,12 +20,14 @@ import com.datageeks.service.EmployeeService;
 import com.datageeks.service.ServicesException;
 
 @RestController
+@RequestMapping(path="/empapp")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmployeeController {
 
 	@Autowired
 	private EmployeeService employeeService = null;
 
-	@GetMapping(path = "/empapp/employees")
+	@GetMapping(path = "/employees")
 	public ResponseEntity<List<EmployeeBean>> getAll() {
 		List<EmployeeBean> employeeList = null;
 		try {
@@ -35,7 +39,7 @@ public class EmployeeController {
 		return new ResponseEntity<List<EmployeeBean>>(employeeList, HttpStatus.OK);
 	}
 
-	@PostMapping(path = "/empapp/employees")
+	@PostMapping(path = "/employees")
 	public ResponseEntity<EmployeeBean> save(@RequestBody EmployeeBean employeeBean) {
 		try {
 			employeeService.saveOrUpdate(employeeBean);
@@ -46,7 +50,7 @@ public class EmployeeController {
 		return new ResponseEntity<EmployeeBean>(employeeBean, HttpStatus.OK);
 	}
 
-	@PutMapping(path = "/empapp/employees")
+	@PutMapping(path = "/employees")
 	public ResponseEntity<EmployeeBean> update(@RequestBody EmployeeBean employeeBean) {
 		try {
 			employeeService.saveOrUpdate(employeeBean);
@@ -57,7 +61,7 @@ public class EmployeeController {
 		return new ResponseEntity<EmployeeBean>(employeeBean, HttpStatus.OK);
 	}
 
-	@GetMapping(path = "/empapp/employees/{empId}")
+	@GetMapping(path = "/employees/{empId}")
 	public ResponseEntity<EmployeeBean> getById(@PathVariable("empId") Integer empId) {
 		EmployeeBean employee = null;
 		try {
@@ -69,7 +73,7 @@ public class EmployeeController {
 		return new ResponseEntity<EmployeeBean>(employee, HttpStatus.OK);
 	}
 
-	@DeleteMapping(path = "/empapp/employees/{empId}")
+	@DeleteMapping(path = "/employees/{empId}")
 	public ResponseEntity<HttpStatus> deleteById(@PathVariable("empId") Integer empId) {
 		try {
 			employeeService.delete(empId);
